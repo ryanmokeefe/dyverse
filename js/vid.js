f96140a2215468a34cb4ba2b431d3e7ae49b1d48
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-07-28 18:59:29 -0400

    youtube vids added

--- js/vid.js
+++ js/vid.js
@@ -85,26 +85,5 @@ var clearPopout = () => {
 
 
 
-// Create array of vids on page
-
-// create # of vids to display based on screen size
-    // on resize, add/remove if neccessary
-
-// Scroll through array onClick prev/next button
-
-// Change video array shown for each category clicked
-
-var vids = {
-    commercials: [],
-    film: [
-        '<iframe width="560" height="315" src="https://www.youtube.com/embed/Jkai9C3G4_c?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>', 
-        '<iframe width="560" height="315" src="https://www.youtube.com/embed/5cybCrOCp8M?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>', 
-        '<iframe width="560" height="315" src="https://www.youtube.com/embed/vI6RnogzKVM?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
-
-    ],
-
-}
-
-
 
 
