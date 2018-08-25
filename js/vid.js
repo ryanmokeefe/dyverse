3a54c6b1a9dbd866ad8b3a087360f58eff0535ad
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-08-25 14:33:58 -0400

    cleaned up navs, vid page

--- js/vid.js
+++ js/vid.js
@@ -1,6 +1,10 @@
+// var headerTop = $("h1").top.offset();
+
+
 $(window).scroll(function() {
     var h1 = $("h1");
-    if ($(window).scrollTop() >= 200)
+
+    if ($(window).scrollTop() >= 300)
     {
         h1.addClass("h1-bgcolor");
     } else 
@@ -82,49 +86,10 @@ var clearPopout = () => {
     popout.style.display = "none";
 };
 
+////
 
-
-// Explosions
-
-
-$('body').on('click', function(e) {
-    explode(e.pageX, e.pageY);
-  })
-  
-  // explosion construction
-  function explode(x, y) {
-    var particles = 55,
-
-      explosion = $('<div class="explosion"></div>');
-  
-    $('body').append(explosion);
-  
-    explosion.css('left', x - explosion.width() / 2);
-    explosion.css('top', y - explosion.height() / 2);
-  
-    for (var i = 0; i < particles; i++) {
-      
-      var x = (explosion.width() / 2) + rand(200, 250) * Math.cos(2 * Math.PI * i / rand(particles - 10, particles + 10)),
-        y = (explosion.height() / 2) + rand(200, 250) * Math.sin(2 * Math.PI * i / rand(particles - 10, particles + 10)),
-        color = rand(100, 255), 
-
-        elm = $('<div class="particle" style="' +
-          'background-color: rgb(' + color + ', ' +  color + ', ' + color + ') ;' +
-          'top: ' + y + 'px; ' +
-          'left: ' + x + 'px"></div>');
-  
-      if (i == 0) { 
-
-        elm.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
-          explosion.remove(); 
-        });
-      }
-      explosion.append(elm);
-    }
-  }
-  
-  function rand(min, max) {
+function rand(min, max) {
     return Math.floor(Math.random() * (max + 1)) + min;
-  }
+}
 
 
